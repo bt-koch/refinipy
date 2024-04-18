65ab8490178d911ddb6b120ce86071b0e15f3203
bt-koch <bela.koch@hotmail.com>
2024-04-18 16:24:21 +0200

    get CDS data

--- get_data.py
+++ get_data.py
@@ -1,8 +1,10 @@
 import json
 import requests
+import eikon as ek
+import pandas as pd
 
 def test2():
-    print("test2")
+    print("test2 asdf")
 
 def read_meta():
     url = "https://raw.githubusercontent.com/bt-koch/ivolR/main/input/banks.json"
@@ -27,10 +29,31 @@ def ric_equity(banks_meta):
     return ric_list
 
 def ric_cds(ric_equity):
-    ric_cds, error = ek.get_data(ric_list, "TR.CDSPrimaryCDSRic")
+    ric_cds, error = ek.get_data(ric_equity, "TR.CDSPrimaryCDSRic")
     ric_cds = ric_cds["Primary CDS RIC"].to_list()
     ric_cds = [x for x in ric_cds if x != ""]
     return ric_cds
 
+def get_cds(ric_cds, start="2012-01-01", end="2023-06-30"):
+    data = []
+    for cds in ric_cds:
+        try:
+            temp = ek.get_timeseries(cds,
+                                     fields="*",
+                                     start_date=start,
+                                     end_date=end)
+            temp.index.name = "DATE"
+            temp.reset_index(inplace=True)
+            temp = temp[["DATE", "CLOSE"]]
+            temp.loc[:, "RIC"] = cds
+            data.append(temp)
+        except:
+            temp = pd.DataFrame({
+                "DATE": [start],
+                "CLOSE": [None],
+                "RIC": [cds]
+            })
+            data.append(temp)
+    return pd.concat(data)
 
 
