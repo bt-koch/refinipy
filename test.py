12f2c8243d10bd14d05af2aef0d968729ce566ae
bt-koch <bela.koch@hotmail.com>
2024-04-23 17:57:12 +0200

    some simple tests of functions

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("../")
+import get_data
+import config
+import utils
+
+config.connect()
+banks_meta = get_data.read_meta()
+ric_equity = get_data.ric_equity(banks_meta)
+ric_cds = get_data.ric_cds(ric_equity)
+cds = get_data.get_cds(ric_cds)
+utils.export_csv(cds, "dataset_cds")
+p2b = get_data.get_price_to_book(ric_equity)
+utils.export_csv(p2b, "dataset_p2b")
