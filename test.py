8ee8793ecf7716db187486fff94b9368173cb452
bt-koch <bela.koch@hotmail.com>
2024-04-23 17:57:32 +0200

    some simple tests of functions

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ banks_meta = get_data.read_meta()
 ric_equity = get_data.ric_equity(banks_meta)
 ric_cds = get_data.ric_cds(ric_equity)
 cds = get_data.get_cds(ric_cds)
+cds.head
 utils.export_csv(cds, "dataset_cds")
 p2b = get_data.get_price_to_book(ric_equity)
+p2b.head
 utils.export_csv(p2b, "dataset_p2b")
